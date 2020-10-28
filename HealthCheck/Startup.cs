346275cf3028db8aff5144952364898dff9ca5f7
kolyad <kolyad@mail.ru>
2020-10-29 02:30:18 +0300

    Add Health checks JSON output

--- HealthCheck/Startup.cs
+++ HealthCheck/Startup.cs
@@ -20,11 +20,17 @@ namespace HealthCheck
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddHealthChecks()
+                .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com", 100))
+                .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
+                .AddCheck("ICMP_03", new ICMPHealthCheck("www.does-notexist.com", 100));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +66,8 @@ namespace HealthCheck
             }
 
             app.UseRouting();
+
+            app.UseHealthChecks("/hc", new CustomHealthCheckOptions());
 
             app.UseEndpoints(endpoints =>
             {
